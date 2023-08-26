36ae5fdccf88e931a3b070f22f6db1e451b7573b
sazzad <kazisazzad805@gmail.com>
2023-08-26 19:37:43 +0600

    simple dice flutter app

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+void main() {
+  return runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text("Dicee"),
+        backgroundColor: Colors.red,
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber=1;
+  var rightDiceNumber=1;
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    return Center(
+      child: Row(
+        children: [
+
+          Expanded(
+            child: TextButton(
+
+              onPressed: (){
+               fun();
+              },
+              child: Image.asset(
+                'images/dice$leftDiceNumber.png',
+              ),
+
+            ),
+          ),
+
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                fun();
+              },
+              child: Image.asset(
+                'images/dice$rightDiceNumber.png',
+              ),
+
+            ),
+          ),
+
+
+        ],
+      ),
+    );
+  }
+  void fun(){
+    setState(() {
+      rightDiceNumber=Random().nextInt(6)+1;
+      leftDiceNumber=Random().nextInt(6)+1;
+    });
+  }
+}
+
+
+
+
+
+
+
